584b6d64f55c09b48854cdd67dc845def3c3151d
Justice Amoh <justiceamoh@gmail.com>
2015-10-08 13:03:50 -0400

    limited classes for ease on system resources

--- load_data.py
+++ load_data.py
@@ -1,6 +1,9 @@
 # Author: Justice Amoh
 # Description: Python script to load and visualize What's Cooking Data
+# Other sources: used lemmitization code from @DipayanSinhaRoy
+# https://www.kaggle.com/dipayan/whats-cooking/whatscooking-python
 
+import numpy as np
 import pandas as pd
 import re
 import matplotlib.pyplot as plt 
@@ -23,20 +26,17 @@ num_classes = len(all_classes)
 # Get numerical labels for ytrain 
 y_train = labels.transform(data.cuisine)
 
-
-# Get vectorized words for xtrain - Using TfidfVectorizer
-# all_ingred = [val for examples in data.ingredients for val in examples]
-# vectorizer = TfidfVectorizer(min_df=1)
-# x_train = vectorizer.fit_transform(all_ingred)
-
-
-# Using WordNet lemmatizer
+# Vectorization of ingredients Using WordNet lemmatization & Tfid
 data['ingredients_clean_string'] = [' , '.join(z).strip() for z in data['ingredients']]  
 data['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in data['ingredients']]
 
-vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,1), max_df=0.57, analyzer='word', token_pattern=r'\w+')
-x_train = vectorizer.fit_transform(data.ingredients_string).todense()
-# vectorizer.get_feature_names()
+vectorizer  = TfidfVectorizer(stop_words='english', ngram_range=(1,1), max_df=0.57, analyzer='word', token_pattern=r'\w+')
+x_train     = vectorizer.fit_transform(data.ingredients_string).todense()
+ingred_dict = vectorizer.vocabulary_
+
+# limit training data: british, chinese & indian
+idx = np.logical_or(y_train == 1, y_train ==3, y_train==7 )
+
 
 # t-SNE Embedding of cuisine data
 print("Computing t-SNE embedding")
