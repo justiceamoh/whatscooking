dcfe79c5d7aca33f8f12a872081e20620abdc0d7
Justice Amoh <justiceamoh@gmail.com>
2015-11-06 14:16:00 -0500

    added dnn code

--- cuisine_dnn.py
+++ cuisine_dnn.py
@@ -1,5 +1,9 @@
 # Description: Python script to train deep neural network on whatscooking project data
-# Dependencies: load_data.py, 
+# Dependencies: DataInterface, 
+
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
 
 from lasagne.layers import InputLayer
 from lasagne.layers import DenseLayer
@@ -15,3 +19,57 @@ from nolearn.lasagne import NeuralNet
 from nolearn.lasagne import TrainSplit
 from nolearn.lasagne import objective
 
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from DataInterface import DataInterface
+from pylab import *
+
+import theano
+import cPickle as pkl
+
+floatX = theano.config.floatX
+
+
+#==================#
+##  LOADING DATA  ##
+#==================#
+dface  = DataInterface()
+x_train,x_valid,y_train,y_valid = dface.get_traindata(full=True)
+x_test = dface.get_testdata()
+labels = dface.classes
+
+NUM_FEATURES = x_train.shape[1]
+NUM_CLASSES  = len(labels)
+
+
+#==========================#
+##  NETWORK ARCHITECTURE  ##
+#==========================#
+layers=[
+        (InputLayer,     {'shape': (None,1,NUM_FEATURES)}),
+        (DenseLayer,     {'num_units': 1000, 'nonlinearity':relu}),
+        (DropoutLayer,   {'p':0.5}),
+        (DenseLayer,     {'num_units':  500, 'nonlinearity':relu}),
+        (DropoutLayer,   {'p':0.5}),
+        (DenseLayer,     {'num_units':  500, 'nonlinearity':relu}),
+        (DropoutLayer,   {'p':0.5}),
+        (DenseLayer,     {'num_units':  500}),
+        (DropoutLayer,   {'p':0.5}),
+        (DenseLayer,     {'num_units':  256}),
+        (DropoutLayer,   {'p':0.5}),                         
+        (DenseLayer,     {'num_units':NUM_CLASSES, 'nonlinearity':softmax}),
+    ]
+
+net = NeuralNet(
+        layers=layer1,
+        max_epochs=40,
+        update=nesterov_momentum,
+        update_learning_rate=0.001,
+        update_momentum=0.9,
+        train_split=TrainSplit(eval_size=0.25),
+        verbose=1,
+    )
+
+#======================#
+##  NETWORK TRAINING  ##
+#======================#
+net.fit(X_train,y_train)
