4a2e95e9f23e5aba55b2f38f9c17856aa692d0ff
Justice Amoh <justiceamoh@gmail.com>
2015-11-30 15:45:36 -0500

    attempting 2D convnet approach

--- cuisine_convnet.py
+++ cuisine_convnet.py
@@ -8,6 +8,8 @@ from __future__ import print_function, unicode_literals
 from lasagne.layers import InputLayer
 from lasagne.layers import DenseLayer
 from lasagne.layers import DropoutLayer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
 
 from lasagne.nonlinearities import softmax
 from lasagne.nonlinearities import rectify as relu
@@ -41,7 +43,7 @@ floatX = theano.config.floatX
 ##  LOADING DATA  ##
 #==================#
 dface  = DataInterface()
-x_train,x_valid,y_train,y_valid = dface.get_traindata(full=True)
+x_train,x_valid,y_train,y_valid = dface.get_traindata(full=False)
 x_test = dface.get_testdata()
 labels = dface.classes
 
@@ -49,9 +51,9 @@ NUM_FEATURES = x_train.shape[1]
 NUM_CLASSES  = len(labels)
 
 # Convert to theano types
-x_train = x_train.astype(dtype=floatX)
-x_valid = x_valid.astype(dtype=floatX)
-x_test  = x_test.astype(dtype=floatX)
+x_train = np.asarray(x_train.astype(dtype=floatX))
+x_valid = np.asarray(x_valid.astype(dtype=floatX))
+x_test  = np.asarray(x_test.astype(dtype=floatX))
 
 y_train = y_train.astype(dtype=np.int32)
 y_valid = y_valid.astype(dtype=np.int32)
@@ -61,24 +63,45 @@ y_valid = y_valid.astype(dtype=np.int32)
 x_train = np.vstack((x_train,x_valid))
 y_train = np.hstack((y_train,y_valid))
 
+
+## Convert Data to 2D for Conv2D
+INPUT_WIDTH  = 78
+INPUT_HEIGHT = 38
+gap = np.zeros((x_train.shape[0],1))
+x_train = np.append(x_train,gap, axis=-1)
+x_train = x_train.reshape((x_train.shape[0], 1, INPUT_WIDTH, INPUT_HEIGHT)) 
+
+gap = np.zeros((x_test.shape[0],1))
+x_test = np.append(x_test,gap, axis=-1)
+x_test = x_test.reshape((x_test.shape[0], 1, INPUT_WIDTH, INPUT_HEIGHT))
+
+
 #==========================#
 ##  NETWORK ARCHITECTURE  ##
 #==========================#
+
 layers=[
-        (InputLayer,     {'shape': (None,NUM_FEATURES)}),
-        (DenseLayer,     {'num_units':  750, 'nonlinearity':relu}),
+        (InputLayer,     {'shape': (None,1,INPUT_WIDTH,INPUT_HEIGHT)}),
+        (Conv2DLayer,    {'num_filters': 32, 'filter_size':(13,6), 'stride':(2,1), 'nonlinearity':relu}),
+        (MaxPool2DLayer, {'pool_size': (2,2)}),
+        (Conv2DLayer,    {'num_filters': 96, 'filter_size':(3,3), 'nonlinearity':relu}),
+        (MaxPool2DLayer, {'pool_size': (2,2)}),
+        # (Conv2DLayer,    {'num_filters': 64, 'filter_size':(3,3), 'nonlinearity':relu}),
+        # (MaxPool2DLayer, {'pool_size': (1,1)}),        
+        (DenseLayer,     {'num_units': 256}),
         (DropoutLayer,   {'p':0.5}),
-        (DenseLayer,     {'num_units':  500, 'nonlinearity':relu}),
+        (DenseLayer,     {'num_units': 256}),
         (DropoutLayer,   {'p':0.5}),
-        (DenseLayer,     {'num_units':  250}),                     
         (DenseLayer,     {'num_units':NUM_CLASSES, 'nonlinearity':softmax}),
     ]
 
+
 net = NeuralNet(
         layers=layers,
-        max_epochs=15,
-        update=rmsprop,
+        max_epochs=40,
+        update=nesterov_momentum,
         update_learning_rate=0.001,
+        update_momentum=0.9,
         train_split=TrainSplit(eval_size=0.3),
         verbose=1,
     )
@@ -128,8 +151,8 @@ print('Total Accuracy: {0:2.4}%'.format(acc*100))
 #==========================#
 ##  TESTING & SUBMISSION  ##
 #==========================#
-predictions = net.predict(x_test)
-outfile=netfile[:-7] + '_submission.csv'
-dface.make_submission(predictions, filename=outfile)
+# predictions = net.predict(x_test)
+# outfile=netfile[:-7] + '_submission.csv'
+# dface.make_submission(predictions, filename=outfile)
 
 
